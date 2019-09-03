6bc41827617a6d4d818c46c1f9ea927e501c4a4b
ebrahimiaval <m.ebrahimiaval@gmail.com>
2019-09-03 12:09:52 +0430

    commnet

--- src/setup/utility/samplejQueryPlugin.js
+++ src/setup/utility/samplejQueryPlugin.js
@@ -1,3 +1,7 @@
-$.fn.samplePlugin = function () {
-    console.log('selected elements: ', $(this));
-}
+/*
+    // you can define jQuery plugin like this and use in project with out import
+
+    $.fn.samplePlugin = function () {
+        console.log('selected elements: ', $(this));
+    }
+*/
